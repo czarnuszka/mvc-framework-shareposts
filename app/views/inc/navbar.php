137c3640ec633d73221455cfc584c82a1f8e44ab
Gabriela Sygnarowska <gabriela.sygnarowska@outlook.com>
2021-02-11 00:12:19 +0100

    6_8: Deleting Posts

--- app/views/inc/navbar.php
+++ app/views/inc/navbar.php
@@ -1,10 +1,9 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark mb-3">
     <div class="container">
-        <div class="container-fluid">
-            <a class="navbar-brand" href="<?php echo URLROOT; ?>"><?php echo SITENAME; ?></a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
+        <a class="navbar-brand" href="<?php echo URLROOT; ?>"><?php echo SITENAME; ?></a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
 
             <div class="collapse navbar-collapse" id="navbarsExampleDefault">
                 <ul class="navbar-nav me-auto mb-2 mb-md-0">
@@ -18,6 +17,9 @@
 
                 <ul class="navbar-nav ml-auto">
                     <?php if(isset($_SESSION['user_id'])) : ?>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Welcome <?php echo $_SESSION['user_name']; ?></a>
+                        </li>
                         <li class="nav-item">
                             <a class="nav-link" aria-current="page" href="<?php echo URLROOT; ?>/users/logout">Logout</a>
                         </li>
