6e7f96755a6093d6970bd97c29ec4da61efb3a12
Gabriela Sygnarowska <gabriela.sygnarowska@outlook.com>
2021-02-11 00:41:44 +0100

    Fixed session_helper.php and show.php bugs

--- app/helpers/session_helper.php
+++ app/helpers/session_helper.php
@@ -19,6 +19,7 @@ function flash($name = '', $message = '', $class = 'alert alert-success'){
         } elseif(empty($message) && !empty($_SESSION[$name])){
             $class = !empty($_SESSION[$name . '_class']) ? ($_SESSION[$name . '_class']) : '';
             echo '<div class="'. $class .'" id="msg-flash">' . $_SESSION[$name] . '</div>';
+            unset($_SESSION[$name]);
             unset($_SESSION[$name. '_class']);
         }
     }
